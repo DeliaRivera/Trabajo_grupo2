9abebe609d56d3241bcd12a0e82c3983301a86a2
Lourdes Zamora <lourdes.zamora@unitec.edu>
2015-10-30 07:38:40 -0600

    Clase tigo
    by: lou

---
+++ Projecto/src/projecto/Tigo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projecto;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Lourdes Zamora
+ */
+public class Tigo {
+    protected ArrayList<Plan> planes;
+    
+}
