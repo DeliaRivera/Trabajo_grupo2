6f0301b12ca6cac883d05ac0f9a98ed5ef3ae7e0
lourdeszamora <lourdes.zamora@unitec.edu>
2015-10-30 07:34:20 -0600

    ClasePlanIphone
    By: Delia

---
+++ Projecto/src/projecto/PlanIphone.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projecto;
+
+/**
+ *
+ * @author Delia
+ */
+public  class PlanIphone extends Plan {
+    protected  String itunes_email;
+    
+    public PlanIphone(String e){
+        super(" Laura ", 80);
+        this.itunes_email=e;
+    }
+
+    public String getItunes_email() {
+        return itunes_email;
+    }
+    
+
+    @Override
+    public double pagoMensual(int mins, int msgs) {
+       
+          mins*=0.4; 
+          msgs *= 0.1;
+       
+        return 22+mins+msgs;      
+        
+        
+    }
+    @Override
+    public void print(){
+        super.print();
+         System.out.print("itunes e-mail: "+itunes_email);
+    }
+    
+    
+    
+    
+}
